81a5d3d036f4209cb7d661fb55e21f3bfad53340
BigPhil2010 <167675922+BigPhil2010@users.noreply.github.com>
2025-02-20 21:41:11 +0100

    reset js

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-function toggleTilemap(mapArray) {
-    let tilemapContainer = document.getElementById("gameTilemap");
-    
-    if (!tilemapContainer) {
-        console.error("Element mit ID 'gameTilemap' nicht gefunden.");
-        return;
-    }
-    
-    tilemapContainer.innerHTML = ""; // mach tile map von davor leer
-    
-    for (let row of mapArray) {
-        let rowDiv = document.createElement("div");
-        rowDiv.style.display = "flex";
-        
-        for (let tile of row) {
-            let img = document.createElement("img");
-            img.src = `tiles/${tile}.png`; // Passe Pfad an 
-            img.style.width = "32px";
-            img.style.height = "32px";
-            rowDiv.appendChild(img);
-        }
-        
-        tilemapContainer.appendChild(rowDiv);
-    }
+function toggleTilemap() {
+    document.getElementById("gameTilemap").classList.toggle("hidden")
 }
 
 // Beispiel-Tilemap 16 mal 12
@@ -36,5 +14,5 @@ let mapData = [
 ];
 
 function loadLevel() {
-    toggleTilemap(mapData);
+    
 }
