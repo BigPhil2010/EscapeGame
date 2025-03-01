93271c19b06fdae9d64f44ddb84327993edf1e29
BigPhil2010 <167675922+BigPhil2010@users.noreply.github.com>
2025-03-01 20:49:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function loadLevel() {
                 for (let x = 0; x < mapData[y].length; x++){
                     let tile = mapData[y][x]
                     document.getElementById(`${y + 1}/${x + 1}`).src = pathData[tile]
+                    console.log(`load ${y + 1}/${x + 1}`)
                 }
             }
         }
