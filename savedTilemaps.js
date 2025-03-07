fcc671641bd811795eec4fad1a100476aa9f2f44
BigPhil2010 <167675922+BigPhil2010@users.noreply.github.com>
2025-03-07 23:08:17 +0100

    Update savedTilemaps.js

--- savedTilemaps.js
+++ savedTilemaps.js
@@ -18,25 +18,25 @@ const NameDerMap = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ],
     [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
     ]
 ]
 //oben ist eine Beispiel-map zum kopieren. eigentlich alles selbsterklärend
 //
 // Für NameDerMap kannst du alles einsetzen nur nicht 2 mal das selbe
 // Den pfad zu den tiles musst du selber raussuchen und einsetzen
-//Darunter sind 2 maps. die erste ist für den unteren Layer (Hintergrund) und die Zweite für den oberen Layer (Bäume und so)
+//Darunter sind 2 maps. die erste ist für den unteren Layer (Hintergrund) und die Zweite für den oberen Layer (Bäume und so) wenn du im Fordergrund nichts placen willst mach null
 // Die kommas müssen alle so bleiben, falls sie geändert werden funktiuniert auch der Rest nicht mehr.
 //Falls du mehr als 2 Pfade brauchst kommt nach jedem ein Komma außer nach dem Letzten!!!
 //
