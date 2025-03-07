de4745ef09914792dfb3cfbbe7a06e15e11f4f25
MYERSQR <warnalidominik@gmail.com>
2025-03-08 00:00:31 +0100

    tilemaps

--- tilemap.js
+++ tilemap.js
@@ -4,12 +4,24 @@ const GAME_TILE = 16;
 
 const Map1 = [
     [
+< HEAD
         "recources/kenney_tiny-town (compressed)/Tiles/tile_0000.png",//0grass
-        "recources/kenney_tiny-town (compressed)/Tiles/tile_0001.png",//1high grass
-        "recources/kenney_tiny-town (compressed)/Tiles/tile_0016.png",//2tree bottom
-        "recources/kenney_tiny-town (compressed)/Tiles/tile_0004.png",//3tree top
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0016.png",//1treebottom
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0004.png",//2treetop
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0001.png",//3highgrass
         "recources/kenney_tiny-town (compressed)/Tiles/tile_0029.png",//4pilz
-        "recources/kenney_tiny-town (compressed)/Tiles/tile_0017.png"//5unkraut
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0017.png",//5unkraut
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0055.png",//6fenceupwards
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0043.png",//7fencetopleftcorner
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0045.png",//8fencetoprightcorner
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0044.png",//9fncehorizontal
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0069.png",//10fencebottomrightcorner
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0067.png",//11fencebottomleftcorner
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0068.png",//12fencedoor
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0057.png",//13fencedownwards
+
+
+>
     ],
     [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,18 +38,18 @@ const Map1 = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ],
     [
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, 4, null, null, null, null, null, null, null, null, null, null, 4, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, 3, null, null, null, null, null, null],
-        [null, null, null, 5, null, null, null, null, null, 2, null, null, 5, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, 4, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+        [null, null, null, null, null, 2, null, null, null, null, null, null, null, null, 2, null],
+        [null, 4, null, null, null, null, 1, null, null, 2, null, 1, 4, null, 1, null],
+        [null, null, null, null, null, null, null, null, 2, 1, null, 2, null, 2, null, 2],
+        [null, null, null, null, null, null, null, null, 1, null, null, 1, null, 1, null, 1],
+        [null, 2, null, null, null, null, null, null, null, 2, null, 2, null, null, null, null],
+        [null, 1, null, 5, null, null, null, null, null, 1, null, 1, 5, null, 2, null],
+        [7, 9, 9, 9, 9, 9, 9, 9, 9, 8, null, 2, null, null, 1, null],
+        [6, null, null, null, null, null, null, null, null, 13, null, 1, null, null, null, null],
+        [6, null, null, null, null, null, null, null, null, 13, null, 2, null, null, null, null],
+        [6, null, null, null, null, null, null, null, null, 13, null, 1, 2, null, 4, null],
+        [6, null, null, null, null, null, null, null, null, 13, null, null, 1, null, null, null],
+        [11, 9, 12, 9, 9, 9, 9, 9, 9, 10, null, null, null, null, null, null]
     ]
 ]
 
