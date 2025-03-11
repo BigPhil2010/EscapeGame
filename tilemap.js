93c3770d1edaf7776862b8696b27657940238ba3
BigPhil2010 <167675922+BigPhil2010@users.noreply.github.com>
2025-03-11 17:07:18 +0100

    Update tilemap.js

--- tilemap.js
+++ tilemap.js
@@ -18,6 +18,23 @@ const Map1 = [
         "recources/kenney_tiny-town (compressed)/Tiles/tile_0068.png",//11fencebottomleftcorner
         "recources/kenney_tiny-town (compressed)/Tiles/tile_0069.png",//12fencedoor
         "recources/kenney_tiny-town (compressed)/Tiles/tile_0058.png",//13fencedownward
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0109.png",//14Boden
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0104.png",//15brunnen unten
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0092.png",//16 brunnen oben
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0048.png",//17 Dach OL
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0057.png",//18 Wagen
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0051.png",//19 Dach OR
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0050.png",//20 Dach OMR
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0049.png",//21 Dach OML
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0060.png",//22 Dach UL
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0061.png",//23 Dach UML
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0062.png",//24 Dach UMR
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0063.png",//25 Dach UR
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0076.png",//26 Haus OL
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0077.png",//27 Haus OML
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0079.png",//28 Dach OL
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0088.png",//29 Fenster
+        "recources/kenney_tiny-town (compressed)/Tiles/tile_0089.png"//30 Tür
     ],
     [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,7 +46,7 @@ const Map1 = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 109, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ],
@@ -41,10 +58,10 @@ const Map1 = [
         [null, 2, null, null, null, null, null, null, null, 2, null, 2, null, null, null, null],
         [null, 1, null, 5, null, null, null, null, null, 1, null, 1, 5, null, 2, null],
         [7, 9, 9, 9, 9, 9, 9, 9, 9, 8, null, 2, null, null, 1, null],
-        [6, 48, 49, 50, 51, null, null, null, null, 13, null, 1, null, null, null, null],
-        [6, 60, 61, 62, 63, null, null, 57, null, 13, null, 2, null, null, null, null],
-        [6, 76, 77, 88, 79, null, 92, null, null, 13, null, 1, 2, null, 4, null],
-        [6, 76, 89, 77, 79, null, 104, null, null, 13, null, null, 1, null, null, null],
+        [6, 17, 21, 20, 19, null, null, null, null, 13, null, 1, null, null, null, null],
+        [6, 22, 23, 24, 25, null, null, 18, null, 13, null, 2, null, null, null, null],
+        [6, 26, 27, 29, 28, null, 16, null, null, 13, null, 1, 2, null, 4, null],
+        [6, 26, 30, 27, 28, null, 15, null, null, 13, null, null, 1, null, null, null],
         [11, 9, 12, 9, 9, 9, 9, 9, 9, 10, null, null, null, null, null, null]
     ]
 ]
